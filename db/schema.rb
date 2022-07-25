2a2741101176e4795607e2b93954b34ef52ce3eb
shunfski <fengshun_ios@126.com>
2022-07-26 00:02:46 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_055355) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_25_154007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,9 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_22_055355) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "validation_codes", force: :cascade do |t|
     t.string "email"
-    t.string "kind"
+    t.integer "kind", default: 1, null: false
+    t.string "code", limit: 50
     t.datetime "used_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
